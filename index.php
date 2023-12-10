47cd321fbe7fae4fa867368b25c66b883223b1c1
CoolIvanData <139839742+CoolIvanData@users.noreply.github.com>
2023-12-10 19:29:12 +0500

    002-v001-10.12.2023

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-phpinfo();
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>WebHouse | Главная страница</title>
+    </head>
+    <body>
+        <h1>
+            WebHouse
+        </h1>
+    </body>
+</html>
